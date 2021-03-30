b466646d785c94bee9f37142af9a1eff2f1cda3f
Rob Kramlich <67026814+KramlichMHS@users.noreply.github.com>
2021-03-30 11:45:33 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,74 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":return img`
+. . . . . 1 1 . . . . . 
+. . . . 1 . . 1 . . . . 
+. . . . 1 . . 1 . . . . 
+. . . 1 . . . . 1 . . . 
+. . . 1 . . . . 1 . . . 
+. . 1 . . . . . . 1 . . 
+. . 1 . . . . . . 1 . . 
+. . 1 . . . . . . 1 . . 
+. 1 . . . . . . . . 1 . 
+. 1 . . 1 1 1 1 . . 1 . 
+1 . 1 1 . . . . 1 1 . 1 
+1 1 . . . . . . . . 1 1 
+`;
+            case "image2":return img`
+1 
+1 
+`;
+            case "image3":return img`
+. . . . 1 1 1 1 1 1 1 . . . . . 
+. . . 1 1 . . . . . 1 1 1 . . . 
+. . 1 1 . . . . . . . . 1 1 . . 
+. . 1 . . . . . . . . . . 1 1 . 
+. 1 1 . . . . . . . . . . . 1 . 
+1 1 . . . . . . . . . . . . 1 1 
+1 . . . . . . . . . . . . . . 1 
+1 . . . . . . . . . . . . . 1 1 
+1 . . . . . . . . . . . . 1 1 . 
+1 1 . . . . . . . . . . 1 1 . . 
+. 1 . . . . . . . . . . 1 1 . . 
+. 1 . . . . . . . . . . . 1 1 . 
+. 1 . . . . . . . . . . . . 1 . 
+. 1 . . . 1 1 1 1 . . . . 1 1 . 
+. 1 1 . 1 1 . . 1 . . . 1 1 . . 
+. . 1 1 1 . . . 1 1 1 1 1 . . . 
+`;
+            case "image4":return img`
+. . 1 1 1 . . . 
+. 1 1 . 1 1 . . 
+1 1 . . . 1 1 . 
+1 . . . . . 1 1 
+1 . . . . . . 1 
+1 1 . . . . 1 1 
+. 1 . . . 1 1 1 
+. 1 1 1 1 1 . . 
+`;
+            case "image5":return img`
+. . 1 1 1 . . . 
+. 1 1 . 1 1 . . 
+1 1 . . . 1 1 . 
+1 . . . . . 1 1 
+1 . . . . . . 1 
+1 1 . . . . 1 1 
+. 1 . . . 1 1 1 
+. 1 1 1 1 1 . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,136 @@
+namespace SpriteKind {
+    export const Enemy2 = SpriteKind.create()
+}
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vy += -5
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        1 
+        1 
+        `, mySprite, 0, -100)
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vx += -5
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vx += 5
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vy += 5
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy2, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.disintegrate, 500)
+    sprite.destroy()
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy2, function (sprite, otherSprite) {
+    for (let value of list) {
+        value.destroy()
+    }
+    list = []
+    mySprite.setPosition(80, 60)
+    mySprite.setVelocity(0, 0)
+    info.changeLifeBy(-1)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    smallRock = sprites.create(img`
+        . . 1 1 1 . . . 
+        . 1 1 . 1 1 . . 
+        1 1 . . . 1 1 . 
+        1 . . . . . 1 1 
+        1 . . . . . . 1 
+        1 1 . . . . 1 1 
+        . 1 . . . 1 1 1 
+        . 1 1 1 1 1 . . 
+        `, SpriteKind.Enemy2)
+    smallRock.setPosition(otherSprite.x, otherSprite.y)
+    smallRock.setVelocity(randint(-25, 25), randint(-25, 25))
+    list.push(smallRock)
+    otherSprite.destroy(effects.disintegrate, 500)
+    sprite.destroy()
+    numRocks += -1
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    for (let value of list) {
+        value.destroy()
+    }
+    list = []
+    mySprite.setPosition(80, 60)
+    mySprite.setVelocity(0, 0)
+    info.changeLifeBy(-1)
+})
+let bigRock: Sprite = null
+let smallRock: Sprite = null
+let projectile: Sprite = null
+let list: Sprite[] = []
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    . . . . . 1 1 . . . . . 
+    . . . . 1 . . 1 . . . . 
+    . . . . 1 . . 1 . . . . 
+    . . . 1 . . . . 1 . . . 
+    . . . 1 . . . . 1 . . . 
+    . . 1 . . . . . . 1 . . 
+    . . 1 . . . . . . 1 . . 
+    . . 1 . . . . . . 1 . . 
+    . 1 . . . . . . . . 1 . 
+    . 1 . . 1 1 1 1 . . 1 . 
+    1 . 1 1 . . . . 1 1 . 1 
+    1 1 . . . . . . . . 1 1 
+    `, SpriteKind.Player)
+let numRocks = 0
+list = []
+info.setScore(0)
+info.setLife(3)
+game.onUpdateInterval(5000, function () {
+    if (numRocks < 3) {
+        bigRock = sprites.create(img`
+            . . . . 1 1 1 1 1 1 1 . . . . . 
+            . . . 1 1 . . . . . 1 1 1 . . . 
+            . . 1 1 . . . . . . . . 1 1 . . 
+            . . 1 . . . . . . . . . . 1 1 . 
+            . 1 1 . . . . . . . . . . . 1 . 
+            1 1 . . . . . . . . . . . . 1 1 
+            1 . . . . . . . . . . . . . . 1 
+            1 . . . . . . . . . . . . . 1 1 
+            1 . . . . . . . . . . . . 1 1 . 
+            1 1 . . . . . . . . . . 1 1 . . 
+            . 1 . . . . . . . . . . 1 1 . . 
+            . 1 . . . . . . . . . . . 1 1 . 
+            . 1 . . . . . . . . . . . . 1 . 
+            . 1 . . . 1 1 1 1 . . . . 1 1 . 
+            . 1 1 . 1 1 . . 1 . . . 1 1 . . 
+            . . 1 1 1 . . . 1 1 1 1 1 . . . 
+            `, SpriteKind.Enemy)
+        bigRock.setPosition(randint(20, 140), -5)
+        bigRock.setVelocity(randint(-15, 15), randint(-15, 15))
+        numRocks += 1
+        list.push(bigRock)
+    }
+})
+forever(function () {
+    if (mySprite.x <= -10) {
+        mySprite.setPosition(170, Math.abs(120 - mySprite.y))
+    } else if (mySprite.x >= 170) {
+        mySprite.setPosition(-10, Math.abs(120 - mySprite.y))
+    } else if (mySprite.y <= -10) {
+        mySprite.setPosition(Math.abs(160 - mySprite.x), 130)
+    } else if (mySprite.y >= 130) {
+        mySprite.setPosition(Math.abs(160 - mySprite.x), -10)
+    }
+})
+forever(function () {
+    for (let value of list) {
+        if (value.x <= -10) {
+            value.setPosition(170, Math.abs(120 - value.y))
+        } else if (value.x >= 170) {
+            value.setPosition(-10, Math.abs(120 - value.y))
+        } else if (value.y <= -10) {
+            value.setPosition(Math.abs(160 - value.x), 130)
+        } else if (value.y >= 130) {
+            value.setPosition(Math.abs(160 - value.x), -10)
+        }
+    }
+})
